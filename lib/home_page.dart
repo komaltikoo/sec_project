38da055cb2c12940cd1e7fef5e22fdb674268e4a
komaltikoo <95487568+komaltikoo@users.noreply.github.com>
2023-05-17 14:13:17 +0530

    final

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_null_comparison
-
 import 'package:flutter/material.dart';
 import 'package:sec_project/GameTv.dart';
 import 'package:sec_project/user_api.dart';
@@ -10,7 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // List<User> _users = [];
   GameTv userResponse = GameTv();
 
   var isLoaded = false;
